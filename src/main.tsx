7e6e0a95797c32b94b1593e229c83a93cb9c26e2
Nick S <modexezy@gmail.com>
2023-02-16 21:51:22 +0100

    chore: cleanup test styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,8 @@ import { createRoot } from 'react-dom/client'
 
 import './styles.css'
 
-import styles from './main.module.css'
-
 createRoot(document.querySelector('#root')!).render(
 	<StrictMode>
-		<div className={styles.example}>whosout dashboard</div>
+		<div>whosout dashboard</div>
 	</StrictMode>,
 )
